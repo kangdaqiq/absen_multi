ba7e6b691786e62cafe1d0bf2198b3343598d9ea
kangdaqiq <kangdaqiq@gmail.com>
2026-05-10 21:58:42 +0700

    fix: convert grouped Collection to array sebelum dikirim ke finalAbsenceReport

--- app/Console/Commands/AutoBolosCommand.php
+++ app/Console/Commands/AutoBolosCommand.php
@@ -182,7 +182,7 @@ class AutoBolosCommand extends Command
         // Use template
         $message = WhatsAppMessageTemplates::finalAbsenceReport(
             totalAbsent: $absentStudents->count(),
-            absentStudentsGrouped: $grouped
+            absentStudentsGrouped: $grouped->toArray()
         );
 
         // Queue message to all classes
